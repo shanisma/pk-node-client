93988373dba77e878bfc351edc0a40eda8a19cf0
shan <shanmugathas.vigneswaran@outlook.fr>
2020-05-02 00:47:18 +0200

    feat(main_sprinklers.py): first release

--- main_sprinklers.py
+++ main_sprinklers.py
@@ -10,6 +10,7 @@ Author : Shanmugathas Vigneswaran
 mail : shangmuathas.vigneswaran@outlook.fr
 
 """
+__version__ = '0.0.1-beta'
 import gc
 import time
 from boot import SSID
@@ -20,34 +21,32 @@ from utils import fit
 from pk import PlantKeeper
 import node_type
 
-
+# Node and Api Gateway setting
 NODE_TYPE = node_type.SPRINKLER
 SPRINKLER_TAG = 'orchid'
 PK_API_GATEWAY_HOST = '10.3.141.1'
 PK_API_GATEWAY_PORT = 8001
-POWER_COLOR = TFT.GREEN
-
+# Soil humidity sensors setting
 SOIL_MOISTURE = ADC(Pin(34))
 SOIL_MOISTURE.atten(ADC.ATTN_11DB)
 SOIL_MOISTURE_FIT = fit(
-    # MAP Analog read min/max
-    [2300, 1110],
+    # Map analog read min/max
+    [2300, 1360],
     # to 0% to 100%
     [0, 100]
 )
-
+# Relay for valve power on / power off
 RELAY = Pin(26, Pin.OUT)
 
-spi = SPI(
-    2,
-    baudrate=20000000,
-    polarity=0,
-    phase=0,
-    sck=Pin(14),
-    mosi=Pin(13),
-    miso=Pin(12)
+SPI = SPI(
+    2, baudrate=20000000,
+    polarity=0, phase=0,
+    sck=Pin(14), mosi=Pin(13), miso=Pin(12)
 )
-tft = TFT(spi, 16, 17, 18)
+POWER_COLOR = TFT.GREEN
+
+# Print Node information / Static section
+tft = TFT(SPI, 16, 17, 18)
 tft.initb2()
 tft.rgb(True)
 tft.fill(TFT.BLACK)
@@ -60,43 +59,64 @@ tft.text((2, 30), "NodeType:", TFT.BLACK, sysfont, 1.1, nowrap=False)
 tft.text((2, 40), NODE_TYPE, TFT.BLACK, sysfont, 1.1, nowrap=False)
 tft.text((2, 50), "Tag: " + SPRINKLER_TAG, TFT.BLACK, sysfont, 1.1, nowrap=False)
 
+# Create Plant Keeper Client
 pk = PlantKeeper(
     host=PK_API_GATEWAY_HOST,
     port=PK_API_GATEWAY_PORT
 )
 pk.set_node_type(node_type=NODE_TYPE)
-last_power = False
 
+last_power = False
 if __name__ == '__main__':
     while True:
         try:
-            sensor = SOIL_MOISTURE_FIT(SOIL_MOISTURE.read())
-            pk.post({"soil_humidity": sensor, 'tag': SPRINKLER_TAG})
+            # +----------------------------------------------------+
+            # | Read from sensors, convert signal,                 |
+            # | post to Api Gateway                                |
+            # +----------------------------------------------------+
+            # read raw adc
+            raw_adc = SOIL_MOISTURE.read()
+            # fit to [adc max, adc min] [0,100] soil humidity
+            soil_humidity = int(SOIL_MOISTURE_FIT(raw_adc))
+            # limit value between [0-100]
+            if soil_humidity > 100:
+                soil_humidity = 100
+            elif soil_humidity < 0:
+                soil_humidity = 0
+            # Post to Api Gateway sensor value + tag
+            pk.post({"soil_humidity": soil_humidity, 'tag': SPRINKLER_TAG})
 
+            # +----------------------------------------------------+
+            # | Builtin TFT screen processing (dynamic zone)       |
+            # +----------------------------------------------------+
+            # full back ground GREEN = Power ON / RED = Power OFF
             if last_power:
                 POWER_COLOR = TFT.GREEN
             else:
                 POWER_COLOR = TFT.RED
-
+            # Flush dynamic part of screen
             tft.fillrect((0, 50), (128, 160), POWER_COLOR)
+            # Print relevant values for user
             tft.text((2, 50), "Tag: " + SPRINKLER_TAG, TFT.BLACK, sysfont, 1.1, nowrap=False)
-            tft.fillrect((95, 60), (30, 10), POWER_COLOR)
-            tft.text((2, 60), "Soil humidity: " + str(sensor), TFT.BLACK, sysfont, 1.1, nowrap=False)
-            tft.fillrect((40, 70), (30, 10), POWER_COLOR)
-            tft.text((2, 70), "Power: " + str(pk.power), TFT.BLACK, sysfont, 1.1, nowrap=False)
+            tft.text((2, 60), "Raw ADC: " + str(raw_adc), TFT.BLACK, sysfont, 1.1, nowrap=False)
+            tft.text((2, 70), "Soil humidity: " + str(soil_humidity), TFT.BLACK, sysfont, 1.1, nowrap=False)
+            tft.text((2, 80), "Power: " + str(pk.power), TFT.BLACK, sysfont, 1.1, nowrap=False)
 
+            # +----------------------------------------------------+
+            # | Activate / Deactivate relay                        |
+            # +----------------------------------------------------+
             if not last_power:
                 last_power = pk.power
                 RELAY.value(pk.power)
             else:
                 if pk.power != last_power:
                     RELAY.value(pk.power)
-
             last_power = pk.power
+
             gc.collect()
             time.sleep(0.5)
-
         except:
+            RELAY.value(0)
             last_power = 0
             tft.fillrect((95, 60), (30, 10), POWER_COLOR)
             tft.text((2, 60), "ERROR ", TFT.BLACK, sysfont, 2, nowrap=False)
